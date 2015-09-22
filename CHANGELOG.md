90bc57652296ed2e5a3fb9828c57829ea8c2b46a
Jeroen Jacobs <git@jeroenj.be>
2015-09-22 20:23:47 +0200

    Bumps version to v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ subliminal cookbook CHANGELOG
 
 This file is used to list changes made in each version of the subliminal cookbook.
 
+1.1.0
+-----
+* Supports subliminal v1.0 (only)
+
 1.0.1
 -----
 * Makes providers configurable for cron recipe
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'Installs subliminal.'
-version           '1.0.1'
+version           '1.1.0'
 
 depends           'python'
 depends           'cron'
